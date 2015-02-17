248e989b3a2de5ba643955cef8bb465ef6b26c6f
Carl Fredriksson <c@msp.se>
2015-02-17 23:13:50 +0100

    More xmpp testing.

--- XmppTesting/XmppTesting/main.cpp
+++ XmppTesting/XmppTesting/main.cpp
@@ -5,8 +5,38 @@
 
 using boost::asio::ip::tcp;
 
-static const int bufferSize = 1024;
-static const std::string exit_command = "exit";
+const int bufferSize = 1024;
+
+std::string Read_Until(std::shared_ptr<tcp::socket> socket, std::string compareStr)
+{
+	boost::array<char, bufferSize + 1> responseBuffer;
+	boost::system::error_code errorCode;
+	std::string responseStr;
+	std::string subStr;
+
+	while (subStr.compare(compareStr) != 0)
+	{
+		size_t responseLength = socket->read_some(boost::asio::buffer(responseBuffer, 1024), errorCode);
+		if (errorCode)
+		{
+			std::cout << "ERROR CODE: " << errorCode << " - \"" << errorCode.message() << "\"\n" << std::endl;
+			break;
+		}
+
+		responseBuffer[responseLength] = '\0';
+		responseStr += responseBuffer.data();
+		subStr = responseStr.substr(responseStr.length() - compareStr.length());
+	}
+
+	return responseStr;
+}
+
+void End_Connection(std::shared_ptr<tcp::socket> socket)
+{
+	boost::system::error_code error_code;
+	socket->shutdown(boost::asio::ip::tcp::socket::shutdown_both, error_code);
+	socket->close();
+}
 
 int main(int argc, char* args[])
 {
@@ -20,19 +50,16 @@ int main(int argc, char* args[])
 		tcp::resolver resolver(ioService);
 		tcp::resolver::query query(hostAddress, portNumber);
 		tcp::resolver::iterator endpointIterator = resolver.resolve(query);
-		tcp::socket socket(ioService);
+		std::shared_ptr<tcp::socket> socket = std::make_shared<tcp::socket>(ioService);
 
 		// Connect socket
 		std::cout << "Connecting to: " << hostAddress << " " << portNumber << "\n" << std::endl;
-		boost::asio::connect(socket, endpointIterator);
+		boost::asio::connect(*socket, endpointIterator);
 
-		std::string message;
-		boost::array<char, 1024> responseBuffer;
-		boost::system::error_code errorCode;
-		size_t responseLength;
+		std::stringstream stream;
+		std::string responseStr;
 
 		// Initiate xml stream to server
-		std::stringstream stream;
 		stream << "<?xmlversion='1.0'?>" << std::endl;
 		stream << "<stream:stream" << std::endl;
 		stream << "to='msp.se'" << std::endl;
@@ -40,20 +67,27 @@ int main(int argc, char* args[])
 		stream << "xmlns:stream='http://etherx.jabber.org/streams'" << std::endl;
 		stream << "version='1.0'>" << std::endl;
 
+		socket->write_some(boost::asio::buffer(stream.str()));
 		std::cout << "---CLIENT---\n" << stream.str() << std::endl;
-		socket.write_some(boost::asio::buffer(stream.str()));
 
 		// Read server response
-		responseLength = socket.read_some(boost::asio::buffer(responseBuffer), errorCode);
-		std::string responseStr1(responseBuffer.begin(), responseBuffer.end());
-		std::cout << "---SERVER---\n" << responseStr1 << std::endl;
+		responseStr = Read_Until(socket, "</stream:features>");
+		std::cout << "---SERVER---\n" << responseStr << "\n" << std::endl;
+
+		// Send STARTTLS
+		stream.str("");
+		stream.clear();
+		stream << "<starttls" << std::endl;
+		stream << "xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>" << std::endl;
+
+		socket->write_some(boost::asio::buffer(stream.str()));
+		std::cout << "---CLIENT---\n" << stream.str() << std::endl;
 
 		// Read server response
-		responseLength = socket.read_some(boost::asio::buffer(responseBuffer), errorCode);
-		std::string responseStr2(responseBuffer.begin(), responseBuffer.end());
-		std::cout << "---SERVER---\n" << responseStr2 << std::endl;
+		responseStr = Read_Until(socket, "<proceed xmlns=\"urn:ietf:params:xml:ns:xmpp-tls\"/>");
+		std::cout << "---SERVER---\n" << responseStr << "\n" << std::endl;
 
-		socket.close();
+		End_Connection(socket);
 	}
 	catch (std::exception& e)
 	{
