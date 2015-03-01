84efcca89171a64d1fb5901348cdc544b2b008d5
Carl Fredriksson <c@msp.se>
2015-03-01 12:12:32 +0100

    local move of files to new structure, not done yet, will need to fix some vs stuff

---
+++ Windows/NetworkCode/WindowsInterface.cpp
@@ -0,0 +1,18 @@
+#include "WindowsInterface.h"
+#include "ChatInterface.h"
+#include "TokenHandler.h"
+#include <string>
+
+__declspec(dllexport) void testConnect(int serviceType, char* buffer, int bufferCapacity)
+{
+	ChatInterface::TestConnect(serviceType, buffer, bufferCapacity);
+}
+
+__declspec(dllexport) void getMessage(char* messageBuffer, int bufferCapacity)
+{
+	ChatInterface::GetMessage(messageBuffer, bufferCapacity);
+}
+__declspec(dllexport) void parseUrl(char* uri)
+{
+	// TokenHandler::parseUrl(uri);
+}
