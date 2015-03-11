5ad3cf1a95e8f61f22964cd48e15e7658e4f26f8
Glazastik <anton.myrholm@gmail.com>
2015-03-11 13:11:47 +0100

    Now almost looks like a beginning of something

--- Android/OOM/app/src/main/java/se/chalmers/oomproject/oom/ConversationFragment.java
+++ Android/OOM/app/src/main/java/se/chalmers/oomproject/oom/ConversationFragment.java
@@ -34,6 +34,7 @@ public class ConversationFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        rootView.
         return rootView;
     }
 
