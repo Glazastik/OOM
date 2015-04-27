5a27cecc6c21042300eaf2901f2473fbbd538837
Glazastik <anton.myrholm@gmail.com>
2015-04-27 10:52:29 +0200

    Made Person and Account

---
+++ Android/OOM/app/src/main/java/se/chalmers/oomproject/oom/Account.java
@@ -0,0 +1,61 @@
+package se.chalmers.oomproject.oom;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Glazastik on 24/04/15.
+ */
+public class Account {
+    private int id;
+    private int serviceType;
+    private String address;
+
+    private String serviceName;
+    private String logoId, logo2Id;
+
+    public Account(int id, int serviceType, String address){
+        this.id = id;
+        this.serviceType = serviceType;
+        this.address = address;
+
+        switch(serviceType){
+            case 0:
+                this.serviceName = "Google Hangouts";
+                logoId = "google";
+                logo2Id = "googledark";
+                break;
+            case 1:
+                this.serviceName = "IRC";
+                logoId = "irc";
+                logo2Id = "ircdark";
+                break;
+            default:
+                this.serviceName = "Unknown";
+        }
+    }
+
+    public String getServiceName(){
+        return serviceName;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getServiceType(){
+        return serviceType;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public Drawable getLogo(Context c) {
+        return c.getResources().getDrawable(c.getResources().getIdentifier(logoId, "drawable", c.getPackageName()));
+    }
+
+    public Drawable getOtherLogo(Context c) {
+        return c.getResources().getDrawable(c.getResources().getIdentifier(logo2Id, "drawable", c.getPackageName()));
+    }
+}
---
+++ Android/OOM/app/src/main/java/se/chalmers/oomproject/oom/Person.java
@@ -0,0 +1,67 @@
+package se.chalmers.oomproject.oom;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Glazastik on 24/04/15.
+ */
+public class Person {
+    private int id;
+    private String name;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String firstName;
+    private String nickName;
+    private String lastName;
+    private ArrayList<Account> accounts;
+
+    public Person (int id, String name){
+        this.accounts = new ArrayList<>();
+        this.firstName = name;
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addAccount(Account a){
+        accounts.add(a);
+    }
+
+    public ArrayList<Integer> getAccountIds(){
+        ArrayList<Integer> ids = new ArrayList<>();
+        for(Account a : accounts){
+            ids.add(a.getId());
+        }
+        return ids;
+    }
+}
