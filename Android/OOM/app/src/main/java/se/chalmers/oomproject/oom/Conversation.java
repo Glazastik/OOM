cd9908c7e3db89adf5b90172ea2c96a41bbad727
Glazastik <anton.myrholm@gmail.com>
2015-04-28 10:20:31 +0200

    Replaced Contact and IService with Person and Account

--- Android/OOM/app/src/main/java/se/chalmers/oomproject/oom/Conversation.java
+++ Android/OOM/app/src/main/java/se/chalmers/oomproject/oom/Conversation.java
@@ -6,17 +6,12 @@ import java.util.ArrayList;
  * Created by Anton on 2015-03-11.
  */
 public class Conversation {
-    private Contact contact;
+    private Person person;
     private ArrayList<Message> messages;
 
-    public Conversation(Contact c){
-        contact = c;
+    public Conversation(Person p){
+        person = p;
         messages = new ArrayList<>();
-        ArrayList<IService> services = new ArrayList<IService>();
-        services.add(new IService.Steam());
-        for(int i = 0; i < 6; i++){
-            messages.add(new Message("This is message " + (int) (29 * Math.random()),services,c));
-        }
     }
 
     public void addMessage(Message m){
@@ -25,7 +20,7 @@ public class Conversation {
     }
 
     public ArrayList<Message> getMessages(){ return messages; }
-    public Contact getContact(){
-        return contact;
+    public Person getPerson(){
+        return person;
     }
 }
